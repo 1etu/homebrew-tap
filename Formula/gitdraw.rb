8f39c9daddad911779a9ac5828dfcc3077b9d96a
Ege Türker <egeturker34@icloud.com>
2025-12-26 00:14:10 +0300

    bump to v1.1.0

--- Formula/gitdraw.rb
+++ Formula/gitdraw.rb
@@ -1,17 +1,17 @@
 class Gitdraw < Formula
   desc "Draw text and art on your GitHub contribution graph"
   homepage "https://github.com/1etu/gitdraw"
-  version "1.0.0"
+  version "1.1.0"
   license "MIT"
 
   on_macos do
     on_arm do
       url "https://github.com/1etu/gitdraw/releases/download/v#{version}/gitdraw-cli-macos-arm64.zip"
-      sha256 "155c696fd9de01a8aaa1d58378c1465eb0a10ccee1f87ea79999a5c241544083"
+      sha256 "9a166b61adada48c9c9a5b223ed94f38b2bf26ec54bc011441156cdb3625004e"
     end
     on_intel do
       url "https://github.com/1etu/gitdraw/releases/download/v#{version}/gitdraw-cli-macos-amd64.zip"
-      sha256 "a2dabebf2256eae9d15ab86a1e096c3899a380fd15ec9e5a19cea5ecf4650aed"
+      sha256 "83ba4ca0c5c77be7ede8a3e4143acdd7410faa0a907d09c0a4c8f14b737bb76f"
     end
   end
 
@@ -24,6 +24,6 @@ class Gitdraw < Formula
   end
 
   test do
-    assert_match "gitdraw", shell_output("#{bin}/gitdraw --version")
+    assert_match "gitdraw", shell_output("#{bin}/gitdraw --help")
   end
 end
